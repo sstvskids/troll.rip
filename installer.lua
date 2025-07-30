e73d0aeae481385908701bb626265629b2fd0ac0
sstvskids <staavstudios@gmail.com>
2025-07-30 19:56:34 +1000

    "Well a start"

---
+++ installer.lua
@@ -0,0 +1,64 @@
+--[[
+    troll.rip 1.0.0
+    Type: Installer
+
+    by: @stav
+    Forever always will be free, and undetected.
+]]
+
+-- Functions troll.rip needs to work
+local function assert(func, msg, cfunc)
+    if cfunc and not func then
+        warn(msg)
+        getgenv().func = cfunc
+    end
+    if not func then
+        error(msg, 2)
+        return
+    end
+end
+
+assert(cloneref, 'Where is the UD-ness?', function(obj)
+    return obj
+end)
+
+assert(firetouchinterest, 'Bad exec lol', function(plr: Instance, item: Instance, tog: number): Instance -- taken off of kool.aid, ik
+    task.spawn(function()
+        if tog == 0 or tog > 1 then return end
+
+        local suc, res = pcall(function()
+            if not item:FindFirstChildOfClass('TouchTransmitter') then
+                return error('Item does not have a TouchTransmitter: '..item, 2)
+            end
+        end)
+
+        if suc then
+            local collision, cframe = item.CanCollide, item.CFrame
+            item.CanCollide = false
+            item.CFrame = plr.CFrame
+            task.wait()
+            item.CFrame = cframe
+            item.CanCollide = collision
+
+            -- checks to make sure they go somewhere (mhm!! soo good guys)
+            if item.CFrame ~= cframe then
+                item.CFrame = cframe
+            end
+            if item.CanCollide ~= collision then
+                item.CanCollide = collision
+            end
+        end
+    end)
+end)
+
+assert(identifyexecutor, 'Super horrible executor', function()
+    return 'troll.rip 1.0.0'
+end)
+
+assert(fireproximityprompt, 'Bad exec lolz: get Volcano @ volcano.wtf or Nucleus @ nucleus.rip')
+
+for _,v in {'troll.rip', 'troll.rip/games', 'troll.rip/libraries'} do
+    if not isfolder(v) then
+        task.spawn(makefolder, v)
+    end
+end
---
+++ libraries/utils.lua
@@ -0,0 +1,127 @@
+--[[
+    troll.rip 1.0.0
+    type: Utils
+
+    og: @Servant
+    rewritten by: @stav
+
+    Rewritten and optimized for troll.rip's needs.
+]]
+
+repeat task.wait() until game:IsLoaded()
+
+local utils = {
+    funcs = {},
+    cars = {}
+}
+
+local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
+local playersService = cloneref(game:GetService('Players'))
+local workspace = cloneref(game:GetService('Workspace'))
+
+local lplr = playersService.LocalPlayer
+
+local suc, res = pcall(require, replicatedStorage.GasStation:WaitForChild('MaxFuel'))
+if suc then
+    utils.cars = res
+else
+    utils.cars = {
+        ['SUV'] = 1200,
+        ['Striped SUV'] = 1250,
+        ['Van'] = 1350,
+        ['Striped Van'] = 1400,
+        ['Jeep'] = 1300,
+        ['Sedan'] = 1300,
+        ['Camo Jeep'] = 1350,
+        ['Striped Sedan'] = 1350,
+        ['Pickup Truck'] = 1300,
+        ['Foiled Pickup Truck'] = 1350,
+        ['Dune Buggy'] = 1100,
+        ['Camo Dune Buggy'] = 1200,
+        ['Sports Car'] = 1000,
+        ['Striped Sports Car'] = 1050,
+        ['Painted Sports Car'] = 1050,
+        ['Supercar'] = 1000,
+        ['Striped Supercar'] = 1050,
+        ['Police Car'] = 1250
+    }
+end
+
+--[[
+    Anticheat Functions
+]]
+
+utils.funcs.disableAC = function()
+    task.spawn(function()
+        for i,v in game:GetDescendants() do
+            if v:IsA('Folder') and v.Name == 'AE' then
+                v:Destroy()
+            end
+            if v:IsA('LocalScript') and v.Name == 'AntiExploit' then
+                v:Destroy()
+            end
+        end
+    end)
+end
+
+--[[
+    Vehicle Functions
+]]
+
+utils.funcs.getVehicleType = function(vehicle, type)
+    local name = string.split(vehicle.Name, '-')
+    return name[type]
+end
+
+utils.funcs.waitForVehicle = function(vehicle)
+    repeat task.wait() until #vehicle:GetDescendants() >= utils.cars[utils.funcs.getVehicleType(vehicle, 2)]
+end
+
+utils.funcs.getVehicle = function(target)
+    target = target or lplr
+    for _, v in workspace.SpawnedCars:GetChildren() do
+        if string.find(v.Name, target.Name) then
+            return v
+        end
+    end
+
+    return 'N/A'
+end
+
+utils.funcs.spawnVehicle = function(type)
+    replicatedStorage.CarSpawnSystem.RemoteEvents.CarSystem:InvokeServer('Spawn', type, false)
+end
+
+--[[
+    Distance Functions
+]]
+
+utils.funcs.getNearestPlayer = function()
+    local nearest, nearestDistance = nil, math.huge
+    for _, v in playersService:GetPlayers() do
+        if v ~= lplr then
+            local distance = (lplr.Character.PrimaryPart.Position - v.Character.PrimaryPart.Position).Magnitude
+            if distance < nearestDistance then
+                nearest = v
+                nearestDistance = distance
+            end
+        end
+    end
+
+    return nearest
+end
+
+utils.funcs.getNearestVehicle = function()
+    local nearest, nearestDistance = nil, math.huge
+    for _, v in workspace.SpawnedCars:GetChildren() do
+        local distance = (lplr.Character.PrimaryPart.Position - v.PrimaryPart.Position).Magnitude
+        if distance < nearestDistance then
+            nearest = v
+            nearestDistance = distance
+        end
+    end
+
+    return nearest
+end
+
+return utils
